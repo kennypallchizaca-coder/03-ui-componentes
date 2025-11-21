d96a3d0dc6e0463cfc0841df1251cb4171ec5b71
Alex Guaman <kennypallchizaca@gmail.com>
2025-11-21 17:57:04 -0500

    first commit

--- src/app/features/components/SignalBoxComponent/signal-box.ts
+++ src/app/features/components/SignalBoxComponent/signal-box.ts
@@ -10,10 +10,17 @@ import { CommonModule } from '@angular/common';
 })
 export class SignalBoxComponent {
   valor = signal<number>(0);
+  progreso = signal<number>(40);
 
   cambiarValor(event: Event) {
     const input = event.target as HTMLInputElement;
     const nuevoValor = Number(input.value);
     this.valor.set(nuevoValor);
+  }
+
+  actualizarProgreso(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const nuevoValor = Number(input.value);
+    this.progreso.set(nuevoValor);
   }
 }
